83dc0e4cdeec13f5e09a84a1ee23fb6c81559461
EliasParada <eliasparadalozano@gmail.com>
2023-03-08 11:13:54 -0600

    Adding the cv

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-let displayNav = document.getElementById('display-nav');
-let displayBtn = document.getElementById('display-btn');
+// let displayNav = document.getElementById('display-nav');
+// let displayBtn = document.getElementById('display-btn');
 
-displayBtn.addEventListener('click', () => {
-    if (displayNav.style.height == '0px') {
-        displayNav.style.height = 'auto';
-    } else {
-        displayNav.style.height = '0px';
-    }
-});
+// displayBtn.addEventListener('click', () => {
+//     if (displayNav.style.height == '0px') {
+//         displayNav.style.height = 'auto';
+//     } else {
+//         displayNav.style.height = '0px';
+//     }
+// });
